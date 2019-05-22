c3525a1d7a2df625ec235c72c49455c6aaf9c4d5
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-22 16:36:31 -0300

    Add views.

---
+++ src/views/Full.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'full-view'
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Quick.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'quick-view'
+}
+</script>
+
+<style lang="scss">
+
+</style>
