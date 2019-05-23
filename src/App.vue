043aa9b2b807bd90bdb7a1c2be5bb0b076ffda14
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-23 13:15:33 -0300

    Add AddButton component.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  
+@import url("https://fonts.googleapis.com/css?family=Nunito:300,300i,400,400i,700&display=swap&subset=latin-ext");
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  background: $color1;
+}
+
+#app {
+  height: 100%;
+  font-family: $font-family;
+}
+
+// globals
+
+.button {
+  border: none;
+  transition: $quick-ease;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+    filter: $little-light;
+  }
+  &:active {
+    transform: $press;
+    filter: $little-shadow;
+  }
 }
 </style>
---
+++ src/components/AddButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="button add-button" @click="$emit('addClick')">
+    <div class="add">+</div>
+    <p>
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "add-button"
+};
+</script>
+
+<style lang="scss">
+.add-button {
+  display: flex;
+  padding: $button;
+  background: $color5;
+  color: $color1;
+  border-radius: $little-radius;
+  font-weight: normal;
+
+  .add {
+    font-weight: 700;
+    margin-right: 2px;
+  }
+}
+</style>
