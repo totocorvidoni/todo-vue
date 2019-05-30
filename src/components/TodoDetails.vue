c53497c2881220394afccb1d4ad4cb11123c63b6
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-30 13:34:55 -0300

    Finish Notes functionality.

--- src/components/TodoDetails.vue
+++ src/components/TodoDetails.vue
@@ -5,28 +5,63 @@
       <p class="priority">{{ priorityText }}</p>
     </div>
     <p class="description">{{ activeTodo.description }}</p>
-    <div class="due-date">
-      <h2>Due in</h2>
-      <p class="time-left" :class="howSoon(date)">{{ timeLeft(date) }}</p>
-      <p class="due-date">{{ dueDate(date) }}</p>
-    </div>
+    <button class="completed button" @click="onCompleteClick">{{ completeButtonText }}</button>
+    <transition name="fade">
+      <div class="due-date" v-if="!this.activeTodo.completed">
+        <h2 v-if="isDue(date)">Has been due for</h2>
+        <h2 v-else>Due in</h2>
+        <p class="time-left" :class="howSoon(date)">{{ timeLeft(date) }}</p>
+        <p class="due-date">{{ dueDate(date) }}</p>
+      </div>
+    </transition>
     <div class="notes-wrapper">
       <h2>Notes</h2>
       <div class="notes">
-        <p class="note" v-for="note in activeTodo.notes">{{ note }}</p>
+        <div class="note" v-for="(note, index) in activeTodo.notes">
+          <p class="text">{{ note }}</p>
+          <button class="remove button" @click="onRemoveNoteClick(index)">X</button>
+        </div>
       </div>
+      <form class="new-note" @submit.prevent="onNewNote">
+        <textarea name="new-note" v-model="newNote" rows="4"/>
+        <input class="button" type="submit" value="Add Note">
+      </form>
     </div>
-    <p>{{ activeTodo.completed }}</p>
   </div>
 </template>
 
 <script>
-import date from "@/mixins/datesHelper";
+import datesHelper from "@/mixins/datesHelper";
 import { mapGetters } from "vuex";
 
 export default {
   name: "todo-details",
-  mixins: [date],
+  mixins: [datesHelper],
+  data() {
+    return {
+      newNote: ""
+    };
+  },
+
+  methods: {
+    onCompleteClick() {
+      this.$store.commit("toggleTodo", this.activeTodo.id);
+    },
+
+    onNewNote(e) {
+      if (this.newNote !== "") {
+        this.$store.commit("addTodoNote", {
+          todoId: this.activeTodo.id,
+          note: this.newNote
+        });
+      }
+      this.newNote = "";
+    },
+
+    onRemoveNoteClick(index) {
+      this.activeTodo.notes.splice(index, 1);
+    }
+  },
 
   computed: {
     ...mapGetters(["activeTodo", "priorityText"]),
@@ -50,6 +85,12 @@ export default {
           return "none";
           break;
       }
+    },
+
+    completeButtonText() {
+      return this.activeTodo.completed
+        ? "Mark as not completed"
+        : "Mark as completed";
     }
   }
 };
@@ -120,6 +161,14 @@ export default {
     padding: 1em;
   }
 
+  .completed {
+    background: $color-good;
+    color: #fff;
+    border-radius: $regular-radius;
+    font-weight: 700;
+    padding: 0.5em 1em;
+  }
+
   .due-date {
     text-align: center;
 
@@ -128,7 +177,7 @@ export default {
       color: $color5;
       font-size: 1.5em;
       font-weight: 700;
-      margin-bottom: 0.5em;
+      margin-bottom: 5px;
       padding: $button-padding;
       text-transform: capitalize;
     }
@@ -161,12 +210,64 @@ export default {
   }
 
   .note {
+    display: flex;
+    align-content: center;
     background: $color1-light;
     border-radius: $regular-radius;
     color: $color5;
     font-weight: 700;
     margin: 0.25em;
+
+    .text {
+      margin: auto 0;
+      padding: 1em;
+    }
+
+    .remove {
+      display: block;
+      border-top-right-radius: $regular-radius;
+      border-bottom-right-radius: $regular-radius;
+
+      &:active {
+        transform: none;
+      }
+    }
+  }
+
+  .new-note {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    background: $color1;
+    border-radius: $regular-radius;
     padding: 1em;
+    margin: 0.5em auto;
+    width: min-content;
+
+    textarea {
+      background: $color1-light;
+      border: 2px solid transparent;
+      border-radius: $little-radius;
+      color: $color5;
+      padding: 0 0.5em;
+      transition: $quick-ease;
+
+      &:focus {
+        outline: none;
+        border-bottom: 2px solid $color5;
+      }
+    }
+
+    input[type="submit"] {
+      border: none;
+      border-radius: $little-radius;
+      background: $color1;
+      color: $color5;
+      font-weight: 700;
+      margin-left: 0.5em;
+      width: 4em;
+      white-space: normal;
+    }
   }
 }
 </style>
